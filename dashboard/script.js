a17cd95181014bd9d5ffc4ebc74e2b3e58cd5706
ionvop <ionvop@gmail.com>
2026-02-24 16:57:39 +0800

    Delete script.js

--- dashboard/script.js
+++
@@ -1,8 +0,0 @@
-g_initialize();
-
-function g_initialize() {
-    for (let element of document.querySelectorAll("*")) {
-        element.style.setProperty("--test", "test");
-        element.style.removeProperty("--test");
-    }
-}
